77dad5223af0e2785fe6a8fe54bc25e4963e1913
chris <chris@ac150577-6901-3246-a117-cd8547e6756f>
2006-03-18 12:22:39 +0000

    remove unnecessary naming

--- tests/getUserDetails.sql
+++ tests/getUserDetails.sql
@@ -2,10 +2,10 @@
 Connection_id : sufdb
 arguments: name
 </dtml-comment>
-SELECT users.name as name, 
-       users.password as password, 
-       roles.role as role,
-       users.extra1 as extra1,
-       users.extra2 as extra2
+SELECT users.name, 
+       users.password, 
+       roles.role,
+       users.extra1,
+       users.extra2
 FROM users,roles 
 WHERE users.name=<dtml-sqlvar name type="string"> and users.name=roles.name
